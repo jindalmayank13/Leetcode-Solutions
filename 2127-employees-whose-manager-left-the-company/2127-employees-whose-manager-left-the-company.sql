a90dc58428859589cfc7e7e7344796e71f79c153
Mayank Jindal <93427749+jindalmayank13@users.noreply.github.com>
2023-07-12 02:40:56 +0530

    Time: 732 ms (60.36%), Space: 0B (100%) - LeetHub

---
+++ 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
@@ -0,0 +1,19 @@
+SELECT DISTINCT emp.employee_id
+FROM   employees emp
+       LEFT JOIN employees mng
+              ON( emp.manager_id = mng.employee_id )
+WHERE  emp.manager_id IS NOT NULL
+       AND emp.salary < 30000
+       AND mng.employee_id IS NULL
+ORDER  BY emp.employee_id 
+
+
+
+
+
+
+
+
+
+
+
