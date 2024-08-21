be50bb6328ff46ad480634be4c48bfc2c4f75db9
Mayank Jindal <93427749+jindalmayank13@users.noreply.github.com>
2024-08-21 08:30:35 +0530

    Time: 17 ms (67.59%), Space: 11.2 MB (37.94%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.cpp
+++ 0664-strange-printer/0664-strange-printer.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
     int strangePrinter(string s) {
-        int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-        
-        for (int i = n-1; i >= 0; --i) {
-            dp[i][i] = 1;
-            for (int j = i+1; j < n; ++j) {
-                dp[i][j] = dp[i][j-1] + 1;
-                for (int k = i; k < j; ++k) {
-                    if (s[k] == s[j]) {
-                        dp[i][j] = min(dp[i][j], dp[i][k] + (k+1<=j-1 ? dp[k+1][j-1] : 0));
-                    }
-                }
+        int n = s.length();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return Util(0, n - 1, s, dp);
+    }
+
+    int Util(int i, int j, const string& s, vector<vector<int>>& dp) {
+        if (i > j) {
+            return 0;
+        }
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        char firstLetter = s[i];
+        // If the current character is not repeated in the rest of the string
+        int answer = 1 + Util(i + 1, j, s, dp);
+        for (int k = i + 1; k <= j; k++) {
+            // If repeated then update the answer
+            if (s[k] == firstLetter) {
+                // Splitting from i -> k - 1 (remove the last character)
+                // and from k + 1 -> j             
+                int betterAnswer = Util(i, k - 1, s, dp) + Util(k + 1, j, s, dp);
+                answer = min(answer, betterAnswer);
             }
         }
-        return dp[0][n-1];
+        return dp[i][j] = answer;
     }
 };
