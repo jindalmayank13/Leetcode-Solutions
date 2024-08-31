ddebe4f1cac8892062f00abda984db75645a4a73
Mayank Jindal <93427749+jindalmayank13@users.noreply.github.com>
2024-08-31 15:13:54 +0530

    Time: 95 ms (79.44%), Space: 53.5 MB (75.23%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.cpp
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        vector<pair<int,double>>adj[n]; //Creating adjacency list
-        for(int i=0;i<edges.size();i++){
-            adj[edges[i][0]].push_back({edges[i][1],succProb[i]});
-            adj[edges[i][1]].push_back({edges[i][0],succProb[i]});
-        }
-        priority_queue<pair<double,int>>pq; //Use maxHeap for path with the maximum probability
-        pq.push({1.0,start}); //{probability,node}
-        vector<double>dist(n,INT_MIN);
-        dist[start]=1;
-        while(!pq.empty()){
-            auto itr=pq.top();
-            pq.pop();
-            double dis=itr.first;
-            int node=itr.second;
-            for(auto it:adj[node]){
-                int adjNode=it.first;
-                double edW=it.second;
-                if(dist[adjNode]<dis*edW){ //If greater probability is found then update probability of adjacent node & push adjacent node in maxHeap
-                    dist[adjNode]=dis*edW;
-                    pq.push({dist[adjNode],adjNode});
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
+        vector<double> maxProb(n, 0.0);
+        maxProb[start_node] = 1.0;
+
+        for (int i = 0; i < n - 1; ++i) {
+            bool updated = false;
+            for (int j = 0; j < edges.size(); ++j) {
+                int u = edges[j][0];
+                int v = edges[j][1];
+                double prob = succProb[j];
+
+                if (maxProb[u] * prob > maxProb[v]) {
+                    maxProb[v] = maxProb[u] * prob;
+                    updated = true;
+                }
+                if (maxProb[v] * prob > maxProb[u]) {
+                    maxProb[u] = maxProb[v] * prob;
+                    updated = true;
                 }
             }
-        }        
-        if(dist[end]==INT_MIN) return 0.00000; //If there is no path from start to end
-        else return dist[end];
+            if (!updated) break;
+        }
+
+        return maxProb[end_node];
     }
 };
