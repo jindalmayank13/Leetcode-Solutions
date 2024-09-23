f8736b0164fb975649df1ebdca0773354ea22e46
Mayank Jindal <93427749+jindalmayank13@users.noreply.github.com>
2024-09-23 16:49:25 +0530

    Time: 119 ms (58.78%), Space: 85.4 MB (60.47%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
@@ -1,31 +1,47 @@
 class Solution {
-public:
-    int minExtraChar(string s, vector<string>& dictionary) {
-        // similar to word break
-        set<string> dict(dictionary.begin(), dictionary.end());
-        vector<int> memo(s.size(), -1);
-        
-        function<int(int)> solve = [&](int i) {
-            if(i == s.size()) return 0;
-            if(memo[i] != -1) return memo[i];
-            
-            // skip this char
-            int result = 1 + solve(i+1);
-            
-            // consider this char
-            string str;
-            
-            for(int j=i; j<s.size(); j++) {
-                str.push_back(s[j]);
-                
-                if(dict.count(str)) {
-                    result = min(result, solve(j+1));
-                }
+private:
+    unordered_map<string, int> um;  // Dictionary words
+    unordered_map<int, int> memo;   // Memoization map for storing dp results
+
+    int solve(string& s, int ind) {
+        // base condition, where recursion gets over
+        if (ind >= s.size()) {
+            return 0;
+        }
+
+        if (memo.find(ind) != memo.end()) {
+            // Return memoized result if already computed
+            return memo[ind];  
+        }
+
+        int extra = INT_MAX;
+        string tmp = "";
+
+        // Try all substrings starting from the current index
+        for (int i = ind; i < s.size(); i++) {
+            tmp += s[i];
+            if (um.find(tmp) != um.end()) {
+                // If the substring is in the dictionary, solve for the remaining part
+                extra = min(extra, solve(s, i + 1));
             }
-            
-            return memo[i] = result;
-        };
+        }
+
+        // Consider the case where the current character is counted as extra
+        // basically skipping the current ind = 0
+        extra = min(extra, 1 + solve(s, ind + 1));
+
+        // Memoize the result for the current index
+        memo[ind] = extra;
         
-        return solve(0);
+        return memo[ind];
+    }
+
+public:
+    int minExtraChar(string s, vector<string>& dict) {
+        for (auto& val : dict) {
+            um[val]++;
+        }
+
+        return solve(s, 0);  // Start the recursion from index 0
     }
 };
